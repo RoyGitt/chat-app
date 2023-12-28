e43325e5576acfd44175f7879b6060c3fc5589af
RoyGitt <officialroy02@gmail.com>
2023-12-28 19:08:03 +0530

    Set up a basic ui for sign in and sign up

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,18 +1,54 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import {
+  Box,
+  Container,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import SignUp from "../componets/SignUp";
+import SignIn from "../componets/SignIn";
 
 const HomePage = () => {
-  const [chats, setChats] = useState([]);
-
-  useEffect(() => {
-    const fetchChat = async () => {
-      const { data } = await axios.get("/api/chats");
-      console.log(data);
-      setChats(data);
-    };
-    fetchChat();
-  }, []);
-
-  return <main>{chats.map((chat) => chat.chatName)}</main>;
+  return (
+    <Container maxW="xl" bg="#333" color="#fff">
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="#000"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text>Chatify</Text>
+      </Box>
+      <Box>
+        <Tabs>
+          <TabList>
+            <Tab>Sign Up</Tab>
+            <Tab>Sign In</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>
+                <SignUp />
+              </p>
+            </TabPanel>
+            <TabPanel>
+              <p>
+                <SignIn />
+              </p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 export default HomePage;
