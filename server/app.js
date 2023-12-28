74f67f4ef9fd5fb024dd274ee63cf57af341fba1
RoyGitt <officialroy02@gmail.com>
2023-12-28 16:12:35 +0530

    Create models namely chat,message and user

--- server/app.js
+++ server/app.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+import express from "express";
+import dotenv from "dotenv";
+import { chats } from "./mockData.js";
+
+dotenv.config();
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.status(200).json({ message: "test" });
+});
+
+app.get("/api/chats", (req, res) => {
+  res.status(200).json(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const id = req.params.id;
+  const filteredData = chats.find((chat) => chat._id === id);
+  res.status(200).json(filteredData);
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
