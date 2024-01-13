10ffaa3aca93b344aa1611ecc7ba5d8b911450cb
RoyGitt <officialroy02@gmail.com>
2024-01-13 13:07:56 +0530

    Create a different UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,6 @@ import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/chats" element={<ChatPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <HomePage />;
 };
 export default App;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,50 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {
-  Box,
-  Container,
-  Text,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
-} from "@chakra-ui/react";
+
 import SignUp from "../components/SignIn";
 import SignIn from "../components/SignUp";
 
 const HomePage = () => {
-  return (
-    <Container maxW="xl" bg="#333" color="#fff">
-      <Box
-        d="flex"
-        justifyContent="center"
-        p={3}
-        bg="#000"
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="lg"
-        borderWidth="1px"
-      >
-        <Text>Chatify</Text>
-      </Box>
-      <Box>
-        <Tabs>
-          <TabList>
-            <Tab>Sign In</Tab>
-            <Tab>Sign Up</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <SignUp />
-            </TabPanel>
-            <TabPanel>
-              <SignIn />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
-    </Container>
-  );
+  return <div>Hello World</div>;
 };
 export default HomePage;
